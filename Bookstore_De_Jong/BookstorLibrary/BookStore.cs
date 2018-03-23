abf486abfed96d761b66bb0ee22c1789e153b4b9
Huibtr <h.teriet1999@gmail.com>
2018-03-23 10:56:13 +0100

    Add files via upload

---
+++ Bookstore_De_Jong/BookstorLibrary/BookStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreLibrary
+{
+    public class BookStore
+    {
+        private string name;
+        private string address;
+        private string city;
+        private string zipcode;
+        private int phonenumber;
+        private string email;
+        private string openingstijden;
+    }
+}
---
+++ Bookstore_De_Jong/BookstorLibrary/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstorLibrary
+{
+    public enum Language
+    {
+        Dutch,
+        Spanish,
+        English,
+        German,
+        French,
+        Unknow
+    }
+
+    public abstract class Product
+    {
+    }
+}
